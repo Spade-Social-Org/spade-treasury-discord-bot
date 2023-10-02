e7baec0e3f094057587cf4356bf4f30b8881ec23
supreme2580 <victoromorogbe69@gmail.com>
2023-10-02 15:38:21 +0100

    suprememods

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ app.use(express.json());
 app.post("/webhook", async (req, res) => {
   const { body, headers } = req;
 
-  console.log("request made")
-
   try {
     Moralis.Streams.verifySignature({
       body,
